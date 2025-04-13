5641bc6c1cfbcfdfb233dbc6999d935e6e1fb73f
char5742 <fujino@stellarcode.jp>
2025-04-13 10:12:21 +0900

    first commit

---
+++ src/main/java/com/example/ec_2024b_back/Ec2024bBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec_2024b_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2024bBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2024bBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ec_2024b_back/Ec2024bBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec_2024b_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2024bBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
