fed18b3be921ae40efacb45358f2f4335dbcc877
char5742 <fujino@stellarcode.jp>
2025-04-29 18:19:43 +0900

    feat: 🎸 authパッケージにInterfaceLayerとInfrastructureLayerのパッケージ情報を追加

---
+++ src/main/java/com/example/ec_2024b_back/auth/api/package-info.java
@@ -0,0 +1,5 @@
+@InterfaceLayer
+package com.example.ec_2024b_back.auth.api;
+
+import org.jmolecules.architecture.layered.InterfaceLayer;
+
---
+++ src/main/java/com/example/ec_2024b_back/auth/infrastructure/package-info.java
@@ -0,0 +1,4 @@
+@InfrastructureLayer
+package com.example.ec_2024b_back.auth.infrastructure;
+
+import org.jmolecules.architecture.layered.InfrastructureLayer;
