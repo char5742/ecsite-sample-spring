65ca22fdd7cb7a112ede29353d0cc994c28619ac
char5742 <fujino@stellarcode.jp>
2025-04-29 21:57:17 +0900

    style: 💄 不要なコメントの削除

--- src/test/java/com/example/ec_2024b_back/utils/JMoleculesRulesUnitTest.java
+++ src/test/java/com/example/ec_2024b_back/utils/JMoleculesRulesUnitTest.java
@@ -6,9 +6,9 @@ import com.tngtech.archunit.lang.ArchRule;
 import org.jmolecules.archunit.JMoleculesArchitectureRules;
 import org.jmolecules.archunit.JMoleculesDddRules;
 
-@AnalyzeClasses(packages = "com.example.ec_2024b_back") // (1)
+@AnalyzeClasses(packages = "com.example.ec_2024b_back")
 class JMoleculesRulesUnitTest {
 
-  @ArchTest ArchRule dddRules = JMoleculesDddRules.all(); // (2)
-  @ArchTest ArchRule onion = JMoleculesArchitectureRules.ensureOnionSimple(); // (2)
+  @ArchTest ArchRule dddRules = JMoleculesDddRules.all();
+  @ArchTest ArchRule onion = JMoleculesArchitectureRules.ensureOnionSimple();
 }
