29e5c496538c4cf7038d34fd219d30b456f04359
char5742 <fujino@stellarcode.jp>
2025-05-03 21:17:35 +0900

    refactor: 💡 package-info.javaから@NullMarkedアノテーションを削除

--- src/main/java/com/example/ec_2024b_back/share/package-info.java
+++ src/main/java/com/example/ec_2024b_back/share/package-info.java
@@ -1,6 +1,3 @@
 @org.springframework.modulith.ApplicationModule(
     type = org.springframework.modulith.ApplicationModule.Type.OPEN)
-@NullMarked
 package com.example.ec_2024b_back.share;
-
-import org.jspecify.annotations.NullMarked;
